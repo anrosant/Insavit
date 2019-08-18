8568064e8d98690b657f762586c5bd11cecbc248
anrosant <anrosant@espol.edu.ec>
2019-08-18 17:23:56 -0500

    UI changes and add reason field in FormData

--- ukuweb/static/js/index.js
+++ ukuweb/static/js/index.js
@@ -1,14 +1,4 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Form from './src/form'
-
-
-function Welcome(props) {
-  return <h1>Hello, {props.name}</h1>;
-}
-
-const element = <Welcome name="world" />;
-ReactDOM.render(
-  <Form />,
-  document.getElementById('form-container')
-);
+$(document).ready(function () {
+  $('#dtBasicExample').DataTable();
+  $('.dataTables_length').addClass('bs-select');
+});
